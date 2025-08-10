103c9920886a703f1dac59e979127a033750dec5
MitchelUlima <90108032+MitchelUlima@users.noreply.github.com>
2025-08-10 17:03:24 -0500

    Update dataBase.js

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,20 +1,28 @@
-﻿import Sequelize from 'sequelize'
-const hostname = 'mitchelmrtp-20251-soft2-db2.postgres.database.azure.com' 
-const username = 'postgres'
-const password = 'Sistemas0912'
-const database = 'soft2'
-const dbPort = 5432
-const dialect = 'postgres'
+import Sequelize from 'sequelize';
+
+const hostname = 'mitchelmrtp-20251-soft2-db2.postgres.database.azure.com';
+const username = 'postgres';
+const password = 'Sistemas0912';
+const database = 'soft2';
+const dbPort = 5432;
+const dialect = 'postgres';
+
 const sequelize = new Sequelize(database, username, password, {
-    host: hostname,
-    port: dbPort,
-    dialect: dialect,
-    operatorAliases: false,
-    pool: {
-        max: 100,
-        min:0,
-        acquire: 20000,
-        idle: 5000
+  host: hostname,
+  port: dbPort,
+  dialect: dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // necesario si no usas certificados de CA locales
     }
-})
+  },
+  pool: {
+    max: 100,
+    min: 0,
+    acquire: 20000,
+    idle: 5000
+  }
+});
+
 export default sequelize;
